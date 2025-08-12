2c10f0a3fc6a331dcf7846fe41c2e2d7abd795b2
Ariel Aharon <xarielah@gmail.com>
2025-08-12 19:21:57 +0300

    feat: updating knowledge dto

--- src/modules/templates/dtos/template-knowledge.dto.ts
+++ src/modules/templates/dtos/template-knowledge.dto.ts
@@ -8,13 +8,13 @@ export class TemplateKnowledgeDto {
 
   @Expose({ name: 'u_name' })
   @ApiProperty({ description: 'Name of the knowledge article' })
-  name: string;
+  title: string;
 
   @Expose({ name: 'u_short_description' })
   @ApiProperty({ description: 'Short description of the knowledge article' })
-  u_short_description: string;
+  description: string;
 
   @Expose({ name: 'u_url' })
   @ApiProperty({ description: 'URL of the knowledge article' })
-  u_url: string;
+  url: string;
 }
